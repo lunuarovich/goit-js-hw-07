24d84f1d8b02de4eac98e8a02e094d8797e16002
lunuarovich <lenuar.max@gmail.com>
2025-11-04 17:48:39 +0200

    hw7

--- js/task-3.js
+++ js/task-3.js
@@ -1,32 +1,8 @@
-class StringBuilder {
-  #value;
+const inputEl = document.querySelector('#name-input');
+const outputEl = document.querySelector('#name-output');
 
-  constructor(initialValue) {
-    this.#value = initialValue;
-  }
+inputEl.addEventListener('input', event => {
+  const name = event.currentTarget.value.trim();
 
-  getValue() {
-    return this.#value;
-  }
-
-  padEnd(str) {
-    this.#value += str;
-  }
-
-  padStart(str) {
-    this.#value = str + this.#value;
-  }
-
-  padBoth(str) {
-    this.#value = str + this.#value + str;
-  }
-}
-
-const builder = new StringBuilder(".");
-console.log(builder.getValue()); // "."
-builder.padStart("^");
-console.log(builder.getValue()); // "^."
-builder.padEnd("^");
-console.log(builder.getValue()); // "^.^"
-builder.padBoth("=");
-console.log(builder.getValue()); // "=^.^="
+  outputEl.textContent = name === '' ? 'Anonymous' : name;
+});
---
+++ js/task-5.js
@@ -0,0 +1,16 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const body = document.querySelector('body');
+const colorSpan = document.querySelector('.color');
+const changeColorBtn = document.querySelector('.change-color');
+
+changeColorBtn.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+
+  body.style.backgroundColor = randomColor; // змінюємо фон
+  colorSpan.textContent = randomColor; // показуємо колір у span
+});
